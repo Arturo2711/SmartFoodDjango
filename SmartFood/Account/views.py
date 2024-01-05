e6afc7cd68166eb43280ec511183ec57a0722dfb
Arturo2711 <arturoca971@gmail.com>
2024-01-05 15:38:34 -0600

    Agregando la carpeta del proyecto Django

---
+++ SmartFood/Account/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from . import forms
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+# Create your views here.
+
+
+def user_login(request):
+    if request.method == 'POST':  # Se espera que la forma sea enviada via POST request
+        # Se crea una instancia de la clase Login form and it's poulate by data from the request
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():  # Validaciones necesairas y adicionales
+            cd = form.cleaned_data
+            # Autenticamos el usuario en los usuarios en la base de datos
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return render(request, 'account/logged_in.html')
+                else:
+                    return HttpResponse('Disabled account')
+        else:
+            return HttpResponse('Invalid login')
+    else:
+        form = forms.LoginForm()
+    return render(request, 'account/login.html', {'form': form})
