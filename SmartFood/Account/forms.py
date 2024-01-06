47c5a525e0880b2af40fe248d1d8f4bb693ee171
Arturo2711 <arturoca971@gmail.com>
2024-01-06 17:37:48 -0600

    Solucionando cambios al modelo User a un modelo personalizado

--- SmartFood/Account/forms.py
+++ SmartFood/Account/forms.py
@@ -1,7 +1,25 @@
 from django import forms
+from .models import CustomUser
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=20)
     password = forms.CharField(widget=forms.PasswordInput)
 
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password',
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password',
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'first_name', 'email',
+                  'edad', 'pesoActual', 'pesoObjetivo']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
--- SmartFood/Account/views.py
+++ SmartFood/Account/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from . import forms
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from . import forms
+from .forms import UserRegistrationForm
 # Create your views here.
 # Just remember, a view is essentially for return a HTTP response
 
@@ -34,3 +34,20 @@ def user_login(request):
 def register_done(request):
     return render(request, 'account/register_done.html')
 
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(request,
+                          'account/register_done.html')
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
